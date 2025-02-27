4dac958d165eaa60b0072df0a23b95e9fda3aba5
madKoding <madkoding@gmail.com>
2025-02-26 22:50:55 -0300

    google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,41 @@
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import Script from "next/script";
 
 import "../styles/main.css";
+import { JSX } from "react/jsx-runtime";
 
-function MyApp({ Component, pageProps, router }: AppProps) {
+/**
+ * Componente principal de la aplicación que envuelve todas las páginas.
+ *
+ * @param {AppProps} props Las propiedades de la aplicación, incluyendo el componente de página, sus props y la ruta actual.
+ * @returns {JSX.Element} El componente renderizado con Google Analytics tracking.
+ */
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
-    <AnimatePresence mode="wait">
-      <Component {...pageProps} key={router.route} />
-    </AnimatePresence>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <AnimatePresence mode="wait">
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    </>
   );
 }
 
